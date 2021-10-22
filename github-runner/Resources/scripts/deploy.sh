69b5cfc702b474f1476b261acda57901af355443
David Liptai <dliptai@swin.edu.au>
2021-10-22 12:01:13 +1100

    initial commit

---
+++ github-runner/Resources/scripts/deploy.sh
@@ -0,0 +1,44 @@
+#!/bin/bash -xeu
+
+GH_RUNNER_NAME="$1"
+GH_REPO_URL="$2"
+GH_TOKEN="$3"
+GH_LABELS="$4"
+GH_REPLACE=false
+
+if $GH_REPLACE; then
+  REPLACE_FLAG="--replace"
+else
+  REPLACE_FLAG=""
+fi
+
+# Change to default home directory
+cd /home/ubuntu
+
+# Wait for auto updates to finish
+while true; do
+  sudo apt install -y -qq jq > /dev/null && break || echo "Waiting 10s for apt..."; sleep 10
+done
+
+# Create a folder
+mkdir actions-runner && cd actions-runner
+
+# Get latest github linux-x64 runner version tag
+GH_VERSION=$(curl -s https://api.github.com/repos/actions/runner/releases | jq -r '.[] | .name' | sort -r | head -1 | cut -d 'v' -f 2)
+GH_FILE="actions-runner-linux-x64-${GH_VERSION}.tar.gz"
+
+# Download the latest runner package
+curl -o "${GH_FILE}" -L "https://github.com/actions/runner/releases/download/v${GH_VERSION}/${GH_FILE}"
+
+# Extract the installer
+tar xzf ./"${GH_FILE}"
+
+# Install and start service
+./config.sh --unattended ${REPLACE_FLAG}\
+  --name "${GH_RUNNER_NAME}" \
+  --url "${GH_REPO_URL}" \
+  --token "${GH_TOKEN}" \
+  --labels "${GH_LABELS}"
+
+sudo ./svc.sh install
+sudo ./svc.sh start
